fc47f5d0ffd252be71b0ead6c7d751de3731de89
Ameliorate <ameliorate2@gmail.com>
2015-02-16 19:17:25 -0500

    Finishes the interpreter
    I still need to add words though. Also the shell

--- 1 FORTH +/src/com/amelorate/ofp/Interpreter.java
+++ 1 FORTH +/src/com/amelorate/ofp/Interpreter.java
@@ -1,5 +1,6 @@
 package com.amelorate.ofp;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Interpreter 
@@ -17,9 +18,73 @@ public class Interpreter
 	
 	public void executeLine(String code)
 	{
-		// I need to first split the line up based on spaces into an array, but keep strings and tables together.
-		// Then I need to check if the current thing is a variable.
-		// If it is then I need to push it to the stack.
-		// Else I need to execute it as a word.
+		String[] codeSplit = splitLine(code);
+		int i = 0;
+		
+		for (int f = 0; f < codeSplit.length; f++);		// I have no idea why I wasn't using for loops sooner.
+		{
+			i++;	// No idea why I can't use f here.
+			Boolean isVar = isVariable(codeSplit[i]);
+			
+			if (isVar == true)
+			{
+				stack.addValue(codeSplit[i]);
+			}
+			
+			else
+			{
+				executeWord(codeSplit[i]);
+			}
+		}
+	}
+	
+	/**
+	 * Executes the string as if it were a word.
+	 * @param word
+	 * The word you want to execute.
+	 */
+	public void executeWord(String word)
+	{
+		words.get(word).Execute(this);
+	}
+	
+	private String[] splitLine(String line)
+	{
+		ArrayList<String> split = new ArrayList<String>();
+		ArrayList<Integer> spacePoses = new ArrayList<Integer>();
+		spacePoses.add(0);
+		
+		for (int i = 0; i < line.length(); i++)
+		{
+			if (line.charAt(i) == ' ')
+			{
+				split.add(line.substring(spacePoses.get(spacePoses.size() + 1), i + 1));	// Basically, it will split the string based on spaces, then add it to split.
+				spacePoses.add(i);	// It then records the position of the space and moves on.
+			}
+		}
+		
+		return (String[]) split.toArray();
+	}
+	
+	private boolean isVariable(String section)
+	{
+		if (section.startsWith("\"") == true || section.startsWith("{") == true || section.startsWith("^") == true)
+			return true;	// Covers everything but numbers.
+		
+		else
+		{
+			boolean isInt = true;
+			try
+			{
+				Integer.parseInt(section);
+			}
+			catch (NumberFormatException e)
+			{
+				isInt = false;
+			}
+			
+			return isInt;	// If the above try catch block threw, then it isn't an int and can be safely assumed to be a word to be executed.
+			// if it didn't, then it is an int.
+		}
 	}
 }

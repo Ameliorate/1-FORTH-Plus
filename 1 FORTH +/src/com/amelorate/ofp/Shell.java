7ddd5dceec69662dedc78c66b0912c7888070db6
Ameliorate <ameliorate2@gmail.com>
2015-02-18 18:49:54 -0500

    Adds a basic shell

--- 1 FORTH +/src/com/amelorate/ofp/Shell.java
+++ 1 FORTH +/src/com/amelorate/ofp/Shell.java
@@ -1,10 +1,36 @@
 package com.amelorate.ofp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Shell 
 {
+	private static final String version = "0.01"; 
 	private static Interpreter interpreter = new Interpreter();
+	private static BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+	private static String input;
 	
 	public static void main(String[] args) {
-		interpreter.executeLine("\"helloWorld\" exmethod");
+		//interpreter.executeLine("\"helloWorld\" exmethod");
+		
+		System.out.print("1 FORTH + Shell " + version + "\n>");
+		
+		while (true)
+		{
+			try 
+			{
+				input = bufferRead.readLine();
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			}
+			
+			interpreter.executeLine(input);
+			System.out.print(">");
+			
+			
+		}
 	}
 }
